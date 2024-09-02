58a3e837595a773489e1139977c149bef90d1fd9
Loganathan1122 <l.komathiloganathan@gmail.com>
2024-09-02 17:14:05 +0530

    Add files via upload

---
+++ FIles.java
@@ -0,0 +1,14 @@
+package com.xx;
+
+public class FIles {
+
+	public static void room1() {
+		System.out.println("hello java");
+		
+
+	}
+	public static void main(String[] args) {
+		FIles.room1();
+	}
+	
+}
